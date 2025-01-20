fd25a00d1c24b51f7b011c0986d234f8435148b4
dragonite-lee <ross1222@naver.com>
2025-01-20 17:56:20 +0900

    docs: husky, lint-staged test를 위한 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <button className="bg-blue-500 text-white rounded hover:bg-blue-700">버튼</button>
-      <div>가나다</div>
+      <div>가나다라마바사</div>
     </div>
   );
 }
