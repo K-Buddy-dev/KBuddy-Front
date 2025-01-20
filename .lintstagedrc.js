033c7ab0110f0aafce66c5ba44860604df118557
dragonite-lee <ross1222@naver.com>
2025-01-20 17:49:47 +0900

    husky, lint-staged, tailwind css 세팅

--- .lintstagedrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    'src/**/*.{ts,tsx}': ['prettier --write', 'eslint --fix'],
-  };
-  
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
-      <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">dddd</button>
+      <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">임시버튼</button>
     </div>
   );
 }
