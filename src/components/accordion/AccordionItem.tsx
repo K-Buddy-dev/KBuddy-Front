c33c6290046782a3f1b7d14538f0253dfde12cd5
장원정 <jang_expedition@gmail.com>
2025-01-29 16:24:45 +0900

    [ADD] import 시 alias 설정 추가

--- src/components/accordion/AccordionItem.tsx
+++ src/components/accordion/AccordionItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cn } from '../../utils/utils.ts';
+import { cn } from '@/utils/utils.ts';
 
 export interface AccordionItemProps {
   children: React.ReactNode;
--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,5 +1,5 @@
 import { cva, VariantProps } from 'class-variance-authority';
-import { cn } from '../../utils/utils';
+import { cn } from '@/utils/utils';
 import React, { useState } from 'react';
 
 const checkboxVariants = cva('flex items-center cursor-pointer', {
