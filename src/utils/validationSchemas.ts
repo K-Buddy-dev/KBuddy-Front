055f52c4d3e64a7c8469dcac8582c7852ebe9d02
장원정 <jang_expedition@gmail.com>
2025-02-05 11:48:25 +0900

    [REFATOR] passwordSchema 메세지 변경

--- src/utils/validationSchemas.ts
+++ src/utils/validationSchemas.ts
@@ -2,16 +2,16 @@ import { z } from 'zod';
 
 export const passwordSchema = z
   .string()
-  .min(8, 'Password must be at least 8 characters long.')
-  .max(20, 'Password must be less than 20 characters long.')
-  .regex(/\d/, 'Password must contain at least one number.')
-  .regex(/[A-Z]/, 'Password must contain at least one uppercase letter.')
-  .regex(/[a-z]/, 'Password must contain at least one lowercase letter.')
-  .regex(/[^\w\s]/, 'Password must contain at least one special character.');
+  .min(8, 'At least 8 characters.')
+  .max(20, 'At most 20 characters.')
+  .regex(/\d/, 'At least one number.')
+  .regex(/[A-Z]/, 'At least one uppercase letter.')
+  .regex(/[a-z]/, 'At least one lowercase letter.')
+  .regex(/[^\w\s]/, 'At least one special character.');
 
 export const passwordValidationRules = [
   { label: 'At least 8 characters', test: (value: string) => value.length >= 8 },
-  { label: 'At least 20 characters', test: (value: string) => value.length <= 20 },
+  { label: 'At most 20 characters', test: (value: string) => value.length <= 20 },
   { label: 'At least one number', test: (value: string) => /\d/.test(value) },
   { label: 'At least one uppercase letter', test: (value: string) => /[A-Z]/.test(value) },
   { label: 'At least one lowercase letter', test: (value: string) => /[a-z]/.test(value) },
