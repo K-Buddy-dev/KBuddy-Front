24df9d354adfe6eea3546d367ac3646ee409ecf0
dragonite-lee <ross1222@naver.com>
2025-02-10 12:30:26 +0900

    feat: 반응형 개발을 위한 임시 bg 위치 변경

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export function DefaultLayout() {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div className="relative w-[360px] h-[768px] px-4">
+    <div className="w-full h-full flex items-center justify-center bg-slate-200">
+      <div className="relative w-[360px] h-[768px] px-4 bg-bg-default">
         <Outlet />
       </div>
     </div>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -23,6 +23,11 @@ export function LoginPage() {
             </form>
           </AccordionItem>
         </Accordion>
+        <div className="relative flex py-5 items-center">
+          <div className="flex-grow border-t border-gray-400"></div>
+          <span className="flex-shrink mx-4 text-gray-400">Content</span>
+          <div className="flex-grow border-t border-gray-400"></div>
+        </div>
       </div>
     </>
   );
