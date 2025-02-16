b7fb507c7ea7bf01d90d5a867fa9ed4d573ae5ff
dragonite-lee <ross1222@naver.com>
2025-02-16 23:21:27 +0900

    feat: 인가코드 확인 및 axios Pull을 위한 커밋

--- src/pages/redirect/KakaoRedirectPage.tsx
+++ src/pages/redirect/KakaoRedirectPage.tsx
@@ -1,5 +1,19 @@
+import { Spinner } from '@/components/spinner';
+import { useSocialTokensStore } from '@/store';
+import { useEffect } from 'react';
+
 export function KakaoRedirectPage() {
+  const { setKakaoToken } = useSocialTokensStore();
   const code = new URL(window.location.href).searchParams.get('code');
   console.log('code: ', code);
-  return <div>dd</div>;
+
+  useEffect(() => {
+    setKakaoToken(code);
+  }, [code]);
+
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <Spinner />
+    </div>
+  );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,2 @@
-export { default as useTabsStore } from './useTabsStore';
+export * from './useTabsStore';
+export * from './useSocialTokensStore';
