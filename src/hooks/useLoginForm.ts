9554365e2fce16dff7d234dec69e0b135b5cf191
장원정 <jang_expedition@gmail.com>
2025-03-10 16:41:06 +0900

    feat: Remember me 기능 추가

--- src/hooks/useLoginForm.ts
+++ src/hooks/useLoginForm.ts
@@ -4,10 +4,11 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
 export const useLoginForm = () => {
+  const emailOrUserId = localStorage.getItem('kBuddyId') || '';
   return useForm<LoginFormData>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
-      emailOrUserId: '',
+      emailOrUserId: emailOrUserId,
       password: '',
     },
   });
