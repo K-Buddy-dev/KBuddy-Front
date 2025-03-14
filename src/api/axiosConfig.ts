0a1a56290a2c9d4248bb589916619ff2e6ca9e74
장원정 <jang_expedition@gmail.com>
2025-03-14 13:56:41 +0900

    feat: api interceptor 등록 - 요청 시 로컬스토리지의 토큰 헤더 설정, 401 응답 시 토큰 재요청 발송

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -7,4 +7,42 @@ const apiClient = axios.create({
   },
 });
 
+const authClient = axios.create({
+  baseURL: 'https://api.k-buddy.kr/kbuddy/v1',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+authClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('kBuddyAccessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+authClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originRequest = error.config;
+    if (error.response.status === '401' && !originRequest._retry) {
+      originRequest._retry = true;
+      try {
+        const { data } = await axios.post('토큰 리프래쉬 주소', {});
+        const { accessToken } = data;
+        localStorage.setItem('kBuddyAccessToken', accessToken);
+        originRequest.headers.Authorization = `Bearer ${accessToken}`;
+        return authClient(originRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default apiClient;
