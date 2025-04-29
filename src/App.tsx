f1a6c847c14ccc1b0936e096d4da98fc045def16
dragonite-lee <ross1222@naver.com>
2025-04-29 14:17:04 +0900

    fix: 배포를 위한 authGuard 활성화

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { SignupVerifyPage } from './pages/SignupVerifyPage.tsx';
 import { SignupFormPage } from './pages/SignupFormPage.tsx';
 import { EmailVerifyGuard } from './components/routes/EmailVerifyGuard.tsx';
 import { EmailVerifyContextProvider } from './components/contexts/EmailVerifyContextProvider.tsx';
-// import { AuthGuard } from './components/routes/AuthGuard.tsx';
+import { AuthGuard } from './components/routes/AuthGuard.tsx';
 import CommunityPostPage from './pages/CommunityPostPage.tsx';
 import { CommunityPage } from './pages/CommunityPage.tsx';
 import { CommunityFormContextProvider } from './components/contexts/CommunityFormContextProvider.tsx';
@@ -24,33 +24,33 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<DefaultLayout />}>
-          {/* <Route element={<AuthGuard />}> */}
-          <Route element={<EmailVerifyContextProvider />}>
-            <Route path="/" element={<LoginPage />} />
-            <Route element={<EmailVerifyGuard guardType="verifyEmail" />}>
-              <Route path="/signup/verify" element={<SignupVerifyPage />} />
+          <Route element={<AuthGuard />}>
+            <Route element={<EmailVerifyContextProvider />}>
+              <Route path="/" element={<LoginPage />} />
+              <Route element={<EmailVerifyGuard guardType="verifyEmail" />}>
+                <Route path="/signup/verify" element={<SignupVerifyPage />} />
+              </Route>
+              <Route element={<EmailVerifyGuard guardType="requireVerified" />}>
+                <Route path="/signup/form" element={<SignupFormPage />} />
+              </Route>
             </Route>
-            <Route element={<EmailVerifyGuard guardType="requireVerified" />}>
-              <Route path="/signup/form" element={<SignupFormPage />} />
+
+            <Route path="/oauth/callback/kakao" element={<KakaoRedirectPage />} />
+            <Route path="/oauth2/code/google" element={<GoogleRedirectPage />} />
+            <Route path="/oauth/callback/apple" element={<AppleRedirectPage />} />
+
+            <Route path="/oauth/signup/form" element={<OauthSignupFormPage />} />
+
+            <Route path="/community" element={<CommunityPage />} />
+            <Route element={<CommunityFormContextProvider />}>
+              <Route path="/community/post" element={<CommunityPostPage />} />
             </Route>
+            <Route path="/home" element={<TempPage />} />
+            <Route path="/service" element={<TempPage />} />
+            <Route path="/message" element={<TempPage />} />
+            <Route path="/profile" element={<TempPage />} />
           </Route>
-
-          <Route path="/oauth/callback/kakao" element={<KakaoRedirectPage />} />
-          <Route path="/oauth2/code/google" element={<GoogleRedirectPage />} />
-          <Route path="/oauth/callback/apple" element={<AppleRedirectPage />} />
-
-          <Route path="/oauth/signup/form" element={<OauthSignupFormPage />} />
-
-          <Route path="/community" element={<CommunityPage />} />
-          <Route element={<CommunityFormContextProvider />}>
-            <Route path="/community/post" element={<CommunityPostPage />} />
-          </Route>
-          <Route path="/home" element={<TempPage />} />
-          <Route path="/service" element={<TempPage />} />
-          <Route path="/message" element={<TempPage />} />
-          <Route path="/profile" element={<TempPage />} />
         </Route>
-        {/* </Route> */}
       </Routes>
     </BrowserRouter>
   );
