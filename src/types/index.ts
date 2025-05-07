33e0eaf1482493752a71bb14b939aedf05c00529
dragonite-lee <ross1222@naver.com>
2025-05-07 21:09:07 +0900

    fix: oauth type 누락 삽입

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,37 +1,4 @@
 export * from './blog';
 export * from './post';
 export * from './qna';
-
-export interface LoginFormData {
-  emailOrUserId: string;
-  password: string;
-}
-
-export interface SignupFormData {
-  firstName: string;
-  lastName: string;
-  email: string;
-  userId: string;
-  password?: string;
-  confirmPassword?: string;
-  birthDate: { year: string; month: string; day: string };
-  country: string;
-  gender: string;
-  oAuthUid?: string | number;
-  oAuthCategory?: 'KAKAO' | 'GOOGLE' | 'APPLE' | null;
-}
-
-export interface OauthRequest {
-  oAuthUid: string | number;
-  oAuthCategory: 'KAKAO' | 'GOOGLE' | 'APPLE' | null;
-}
-
-export type TransitionState = 'current' | 'animating';
-export type TransitionDirection = 'left' | 'right';
-
-export interface Activity {
-  key: string;
-  element: React.ReactNode;
-  transition?: TransitionState;
-  direction?: TransitionDirection;
-}
+export * from './oauth';
