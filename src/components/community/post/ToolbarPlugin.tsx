6956507f9ee8db2aadb9f7c4e1027ae680ea98c9
장원정 <jang_expedition@gmail.com>
2025-06-14 19:28:21 +0900

    fix: TextEditor 텍스트 변환 로직 변경

--- src/components/community/post/ToolbarPlugin.tsx
+++ src/components/community/post/ToolbarPlugin.tsx
@@ -10,28 +10,33 @@ export const ToolbarPlugin = ({ editor }: ToolbarPluginProps) => {
     return null;
   }
 
+  const handleFormat = (type: 'bold' | 'italic' | 'strike') => {
+    if (editor.isActive(type)) {
+      editor.chain().focus().unsetMark(type).run();
+    } else {
+      editor.chain().focus().setMark(type).run();
+    }
+  };
+
   return (
     <div id="toolbar" className="w-full h-auto !border-0 relative pb-4">
       <div className="flex items-center gap-4">
         <button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          disabled={!editor.can().chain().focus().toggleBold().run()}
+          onClick={() => handleFormat('bold')}
           className={editor.isActive('bold') ? 'is-active !w-6 !h-6 !p-0' : '!w-6 !h-6 !p-0'}
           title="볼드"
         >
           <BoldTextIcon fill={editor.isActive('bold') ? '#6952F9' : '#222222'} />
         </button>
         <button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          disabled={!editor.can().chain().focus().toggleItalic().run()}
+          onClick={() => handleFormat('italic')}
           className={editor.isActive('italic') ? 'is-active !w-6 !h-6 !p-0' : '!w-6 !h-6 !p-0'}
           title="이탤릭"
         >
           <ItalicTextIcon fill={editor.isActive('italic') ? '#6952F9' : '#222222'} />
         </button>
         <button
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          disabled={!editor.can().chain().focus().toggleStrike().run()}
+          onClick={() => handleFormat('strike')}
           className={editor.isActive('strike') ? 'is-active !w-6 !h-6 !p-0' : '!w-6 !h-6 !p-0'}
           title="취소선"
         >
