979411426868497bce887f1f59b774f63b669175
장원정 <jang_expedition@gmail.com>
2025-06-14 17:07:02 +0900

    fix: keybaordHeight 관련 로직들 삭제

--- src/components/community/post/ToolbarPlugin.tsx
+++ src/components/community/post/ToolbarPlugin.tsx
@@ -1,28 +1,15 @@
-import { useCallback, useState, useEffect, useRef } from 'react';
-import { cn } from '@/utils/utils';
+import { useCallback, useState, useEffect } from 'react';
 import { BoldTextIcon, ItalicTextIcon, CancelLineTextIcon, ListTextIcon } from '@/components/shared';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { FORMAT_TEXT_COMMAND, $getSelection, $isRangeSelection } from 'lexical';
 import { INSERT_UNORDERED_LIST_COMMAND, INSERT_ORDERED_LIST_COMMAND, $isListNode } from '@lexical/list';
-import { useMobileEnv } from '@/hooks/useMobileEnvContext';
 
-export interface ToolbarPluginProps {
-  keyboardHeight: number;
-  isFocused: boolean;
-}
-
-export const ToolbarPlugin = ({ keyboardHeight, isFocused }: ToolbarPluginProps) => {
+export const ToolbarPlugin = () => {
   const [editor] = useLexicalComposerContext();
   const [isBold, setIsBold] = useState(false);
   const [isItalic, setIsItalic] = useState(false);
   const [isStrikethrough, setIsStrikethrough] = useState(false);
   const [isList, setIsList] = useState(false);
-  const { isMobile, isAndroid } = useMobileEnv();
-
-  const [scrollY, setScrollY] = useState(0);
-  const [initialScrollY, setInitialScrollY] = useState(0);
-  const toolbarRef = useRef<HTMLDivElement>(null);
-  const rafRef = useRef<number>();
 
   const formatText = useCallback(
     (format: 'bold' | 'italic' | 'strikethrough') => {
@@ -57,45 +44,6 @@ export const ToolbarPlugin = ({ keyboardHeight, isFocused }: ToolbarPluginProps)
   }, []);
 
   useEffect(() => {
-    if (isFocused) {
-      // 1. 포커스 시점의 스크롤 위치를 초기값으로 설정
-      const currentScroll = window.scrollY;
-      setInitialScrollY(currentScroll);
-      setScrollY(currentScroll);
-
-      // 2. 스크롤 이벤트 리스너 등록
-      const handleScroll = () => {
-        if (rafRef.current) {
-          cancelAnimationFrame(rafRef.current);
-        }
-
-        rafRef.current = requestAnimationFrame(() => {
-          if (toolbarRef.current) {
-            const currentScroll = window.scrollY;
-            setInitialScrollY((prev) => {
-              if (prev < currentScroll) {
-                return currentScroll;
-              }
-              return prev;
-            });
-            setScrollY(currentScroll);
-          }
-        });
-      };
-
-      window.addEventListener('scroll', handleScroll, { passive: true });
-
-      // 3. Cleanup: 포커스가 해제되면 이벤트 리스너 제거
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-        if (rafRef.current) {
-          cancelAnimationFrame(rafRef.current);
-        }
-      };
-    }
-  }, [isFocused]);
-
-  useEffect(() => {
     editor.registerUpdateListener(({ editorState }) => {
       editorState.read(() => {
         updateToolbar();
@@ -103,38 +51,8 @@ export const ToolbarPlugin = ({ keyboardHeight, isFocused }: ToolbarPluginProps)
     });
   }, [editor, updateToolbar]);
 
-  useEffect(() => {
-    console.log('scrollY', scrollY);
-    console.log('initialScrollY', initialScrollY);
-    console.log('scrollY - initialScrollY', scrollY - initialScrollY);
-  }, [scrollY, initialScrollY]);
-
   return (
-    <div
-      ref={toolbarRef}
-      id="toolbar"
-      className={cn(
-        'w-full h-auto !border-0',
-        isMobile
-          ? isFocused
-            ? `fixed p-4 left-0 bg-white z-50 transition-transform duration-100`
-            : 'hidden'
-          : 'relative pb-4'
-      )}
-      style={
-        isMobile && isFocused
-          ? isAndroid
-            ? {
-                bottom: '0px',
-                transform: `translateY(${scrollY - initialScrollY}px)`,
-              }
-            : {
-                bottom: `${keyboardHeight}px`,
-                transform: `translateY(${scrollY}px)`,
-              }
-          : undefined
-      }
-    >
+    <div id="toolbar" className="w-full h-auto !border-0 relative pb-4">
       <div className="flex items-center gap-4">
         <button
           className="!w-6 !h-6 !p-0"
