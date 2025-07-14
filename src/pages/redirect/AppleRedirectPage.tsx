7d7997d7a6fe2dc16a905c8c834860cfde11aa5e
dragonite-lee <ross1222@naver.com>
2025-07-14 22:30:36 +0900

    fix: 임시 배포

--- src/pages/redirect/AppleRedirectPage.tsx
+++ src/pages/redirect/AppleRedirectPage.tsx
@@ -1,5 +1,5 @@
 import { Spinner } from '@/components/shared/spinner';
-import { useMemberCheckHandler, useOauthLoginHandler, useToast } from '@/hooks';
+import { useMemberCheckHandler, useOauthLoginHandler } from '@/hooks';
 import { useSocialStore } from '@/store';
 import { OauthRequest } from '@/types';
 import { parseJwt } from '@/utils/utils';
@@ -27,7 +27,7 @@ export function AppleRedirectPage() {
   const { setEmail, setoAuthUid, setoAuthCategory, socialStoreReset, setFirstName, setLastName } = useSocialStore();
   const { checkMember, isLoading } = useMemberCheckHandler();
   const { handleLogin } = useOauthLoginHandler();
-  const { showToast } = useToast();
+  // const { showToast } = useToast();
 
   const [memberCheckData, setMemberCheckData] = useState<OauthRequest | null>(null);
   const [isMember, setIsMember] = useState<boolean | null>(null);
@@ -40,6 +40,7 @@ export function AppleRedirectPage() {
 
   const getAppleUserInfo = async () => {
     const searchParams = new URLSearchParams(window.location.search);
+    console.log('searchParams: ', searchParams);
     const userString = searchParams.get('user');
     const user = userString ? JSON.parse(userString) : null;
     const idToken = searchParams.get('id_token');
@@ -52,15 +53,15 @@ export function AppleRedirectPage() {
     try {
       const validatedUserInfo = AppleIdTokenSchema.parse(parseJwt(idToken));
 
-      if (!user?.name?.firstName || !user?.name?.lastName) {
-        showToast({
-          message: 'Name sharing is required to sign up with Apple.',
-          type: 'error',
-          duration: 3000,
-        });
-        navigate('/');
-        return;
-      }
+      // if (!user?.name?.firstName || !user?.name?.lastName) {
+      //   showToast({
+      //     message: 'Name sharing is required to sign up with Apple.',
+      //     type: 'error',
+      //     duration: 3000,
+      //   });
+      //   navigate('/');
+      //   return;
+      // }
 
       setFirstName(user.name.firstName);
       setLastName(user.name.lastName);
