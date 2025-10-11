9c05346a2f803a54949a5a2617fe8e4918dfcfee
이준영 <junyoung@MacBook-Pro.local>
2025-10-11 09:52:58 +0900

    fix: enum 변경

--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -1,10 +1,10 @@
 export const APP_PUSH_TYPE = {
-  PUSH_NOTIFICATION: 'PUSH_NOTIFICATION',
+  PUSH_NOTIFICATION: 'pushNotification',
 } as const;
 
 export const APP_PART = {
-  BLOG: 'BLOG',
-  QNA: 'QNA',
+  BLOG: 'blog',
+  QNA: 'qna',
 } as const;
 
 export const COMMUNITY_APP_TAB = {
@@ -12,7 +12,6 @@ export const COMMUNITY_APP_TAB = {
   [APP_PART.QNA]: 'Q&A',
 } as const;
 
-// 최종 라우팅 URL을 만드는 함수
 export const getAppRoute = (part: keyof typeof COMMUNITY_APP_TAB, postID: string) => {
   const tab = COMMUNITY_APP_TAB[part];
   return `/community/detail/${postID}?tab=${encodeURIComponent(tab)}`;
